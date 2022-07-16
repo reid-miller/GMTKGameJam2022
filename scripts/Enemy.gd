0e73eb070513a43e389105fe9c0601948f86c0e9
mtru32 <mark.truttmann32@gmail.com>
2022-07-16 01:27:02 -0500

    Add card animations

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -4,12 +4,19 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 var health = 2
+onready var obj = get_parent().get_node("Player")
+
+var speed = 10
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _physics_process(delta):
+	pass
+	var dir = (obj.global_position - global_position).normalized()
+	move_and_collide(dir * speed * delta)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
