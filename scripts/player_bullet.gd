72e2f5bfaf5fa103bfe9c48fc21347b29b0fee22
Evan Hecht <evanhecht01@gmail.com>
2022-07-16 15:54:37 -0500

    nearly finished player

--- scripts/player_bullet.gd
+++ scripts/player_bullet.gd
@@ -3,9 +3,19 @@ extends player_hitbox
 var speed = 500
 var velocity: Vector2 = Vector2.ZERO
 
+
+
 func _ready() -> void:
 	lifetime = 1000
 	velocity = velocity.normalized() * speed
+	connect("body_entered", self, "_detect_wall")
+
 
 func _physics_process(delta: float) -> void:
-	global_position += velocity * delta
+	global_position += (velocity - Globals.player_scene.velocity) * delta
+	look_at(position + velocity)
+
+
+func _detect_wall(area: Node):
+	if area is Wall or area is Enemy:
+		queue_free()
