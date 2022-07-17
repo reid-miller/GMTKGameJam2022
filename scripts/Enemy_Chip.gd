8f208adcad88df4bb9b57c47b27e490c3cbb03a3
mtru32 <mark.truttmann32@gmail.com>
2022-07-16 19:35:42 -0500

    Fix death timer

--- scripts/Enemy_Chip.gd
+++ scripts/Enemy_Chip.gd
@@ -3,12 +3,19 @@ extends Enemy
 onready var enemy_sprite: AnimatedSprite = $EnemyBodySprite
 var lap = Globals.lap
 var death_interval: float = 1
+var death_timer: Timer
+var startTime: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_determine_level()
-	enemy_sprite.playing = true
+	death_timer = Timer.new()
+	add_child(death_timer)
+	death_timer.wait_time = death_interval
+	death_timer.connect("timeout", self, "_death")
+	if dir != Vector2.ZERO:
+		enemy_sprite.playing = true
 	
 func _physics_process(delta: float) -> void:
 	_handle_death()
@@ -23,17 +30,14 @@ func _determine_level():
 	elif lap >= 4:
 		enemy_sprite.animation = "Blue"
 
-func _handle_death():
-	var death_timer: Timer = Timer.new()
-	add_child(death_timer)
-	death_timer.wait_time = death_interval
-	death_timer.connect("timeout", self, "_death")
-	
+func _handle_death():	
 	if health <= 0:
 		enemy_sprite.modulate = Color(1,0,0)
 		enemy_sprite.stop()
 		enemy_sprite.flip_v = true
-		death_timer.start()
+		if !startTime:
+			death_timer.start()
+			startTime = true
 
 func _death():
 	queue_free()
