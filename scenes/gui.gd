5ceb6c8b74d2757ee0428a562abd48fb41766532
Reid <48286383+reid-miller@users.noreply.github.com>
2022-07-17 11:57:35 -0500

    gui animation

--- scenes/gui.gd
+++ scenes/gui.gd
@@ -2,16 +2,20 @@ extends Control
 
 
 # Declare member variables here. Examples:
-# var a = 2
+var past_health
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	past_health = Globals.player_scene.health
 	pass # Replace with function body.
 
 
 
 func _process(delta):
 	$AnimatedSprite.frame = Globals.player_scene.health
+	if past_health != Globals.player_scene.health:
+		past_health = Globals.player_scene.health
+		$AnimatedSprite/AnimationPlayer.play()
 
